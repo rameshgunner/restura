199e3906495e099d28a72f323676e7a58a4e69d4
rameshgunner <bykalpt@gmail.com>
2016-06-19 00:17:16 +0545

    form stylesheets updated

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20160616083901) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.string   "username"
   end
 
